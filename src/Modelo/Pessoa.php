5a273fa47beccbc8eacbad31e031667b34f1019e
Felipe Silva <felipe.dacs@gmail.com>
2021-03-02 19:10:20 -0300

    :tv: add aula 18

--- src/Modelo/Pessoa.php
+++ src/Modelo/Pessoa.php
@@ -24,7 +24,7 @@ abstract class Pessoa
         return $this->cpf->recuperaNumero();
     }
 
-    protected function validaNomeTitular(string $nomeTitular)
+    final protected function validaNomeTitular(string $nomeTitular)
     {
         if (strlen($nomeTitular) < 5) {
             echo "Nome precisa ter pelo menos 5 caracteres";
